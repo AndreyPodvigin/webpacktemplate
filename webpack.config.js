d60afa21e1fdb174f9f7b2d033b11b3636bec8ba
AndreyPodvigin <sn.podvigin.andrey@gmail.com>
2022-07-19 09:52:32 +0300

    added React preset to Babel

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
+            presets: ['@babel/preset-env', "@babel/preset-react"],
+            plugins: ['@babel/plugin-transform-runtime'],
           }
         }
       },
